62c2dc1fcc76c9343ab1d4f7659eb539e5ec5381
Shakhnur-Sh <s_shazhabai@kbtu.kz>
2026-02-04 15:06:25 +0600

    Rename folder Practice1 to Practice 01

---
+++ Practice 01/python-basics/Data_types.py
@@ -0,0 +1,58 @@
+x = 5
+print(type(x))
+
+
+x = "Hello World"
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+
+x = 20
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+
+x = 20.5
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+
+x = ["apple", "banana", "cherry"]
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+
+x = ("apple", "banana", "cherry")
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+
+x = {"apple", "banana", "cherry"}
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+
+x = True
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
+
+
+x = str("Hello World")
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
